3ed15bb4a5b885f788ed8140c21bb019d68a8fe3
mdfaiz9 <mdfaizalam827@gmail.com>
2025-01-26 17:56:59 +0530

    folder structure

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MyProfile from "./pages/MyProfile";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
