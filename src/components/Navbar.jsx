6ed7a40d4d5d615670bf10f53d09678bcb64fd78
mdfaiz9 <mdfaizalam827@gmail.com>
2025-01-26 20:59:29 +0530

    complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,121 +1,115 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
+
   return (
     <div className="flex flex-wrap place-items-center ">
       <section className="relative mx-auto">
-        <nav className="flex justify-between bg-yellow-400 text-white w-screen">
+        <nav className="flex justify-between bg-[#006D5B] text-white w-screen">
           <div className="px-5 xl:px-12 py-6 flex w-full items-center">
             {/* Logo */}
-            <a className="text-3xl font-bold font-heading" href="#">
+            <NavLink
+              to="/"
+              className="text-3xl font-bold font-heading text-yellow-400"
+            >
               <img className="h-9" src="/logo.png" alt="Logo" />
-            </a>
+            </NavLink>
 
             {/* Desktop Menu */}
             <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
               <li>
-                <a className="hover:text-gray-200" href="#">
+                <NavLink
+                  to="/"
+                  className="text-yellow-400  text-lg font-semibold"
+                >
                   Home
-                </a>
+                  <hr className="border-none outline-none h-0.5 bg-yellow-400  w-3/5 m-auto hidden" />
+                </NavLink>
               </li>
               <li>
-                <a className="hover:text-gray-200" href="#">
-                  Category
-                </a>
+                <NavLink
+                  to="/about"
+                  className="text-yellow-400 text-lg font-semibold"
+                >
+                  About
+                  <hr className="border-none outline-none h-0.5 bg-yellow-400  w-3/5 m-auto hidden" />
+                </NavLink>
               </li>
+
               <li>
-                <a className="hover:text-gray-200" href="#">
-                  Collections
-                </a>
-              </li>
-              <li>
-                <a className="hover:text-gray-200" href="#">
+                <NavLink
+                  to="/contact"
+                  className=" text-yellow-400 text-lg font-semibold"
+                >
                   Contact Us
-                </a>
+                  <hr className="border-none outline-none h-0.5 bg-yellow-400  w-3/5 m-auto hidden" />
+                </NavLink>
               </li>
             </ul>
 
             {/* Icons */}
             <div className="hidden xl:flex items-center space-x-5">
-              <a
-                className="hover:text-gray-200"
-                href="#"
-                aria-label="Favorites"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              {/* <Link className="hover:text-yellow-400" aria-label="User Profile"> */}
+              {token ? (
+                <div className="cursor-pointer group relative">
+                  <img
+                    className="w-10 rounded-full"
+                    src="assets/profileImage.jpg"
+                    alt="profile"
                   />
-                </svg>
-              </a>
-              <a
-                className="hover:text-gray-200"
-                href="#"
-                aria-label="Shopping Cart"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+                  <div className="absolute top-0 right-0 pt-14 text-base font-medium text-yellow-400  z-20 hidden group-hover:block">
+                    <div className="min-w-48 bg-[#006D5B] rounded flex flex-col gap-4 py-4 px-2">
+                      <NavLink to="/my-profile">
+                        <p className="hover:bg-yellow-400 hover:text-black cursor-pointer px-3 rounded-full">
+                          My Profile
+                        </p>
+                      </NavLink>
+                      <NavLink to="/mycomplains">
+                        <p className="hover:bg-yellow-400 hover:text-black cursor-pointer px-3 rounded-full">
+                          My Complains
+                        </p>
+                      </NavLink>
+                      <NavLink to="/logout">
+                        <p className="hover:bg-yellow-400 hover:text-black cursor-pointer px-3 rounded-full">
+                          Sign Out
+                        </p>
+                      </NavLink>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <button
+                  onClick={() => navigate("/register")}
+                  className="bg-yellow-400 text-black px-8 py-3 rounded-full font-semibold"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-              </a>
-              <a
-                className="hover:text-gray-200"
-                href="#"
-                aria-label="User Profile"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </a>
+                  Create Account
+                </button>
+              )}
+              {/* </Link> */}
             </div>
           </div>
 
           {/* Mobile Menu Button */}
           <div className="xl:hidden flex items-center">
             <button
-              className="navbar-burger self-center mr-12"
+              className="navbar-burger self-center mr-12 text-yellow-400"
               onClick={toggleMobileMenu}
               aria-label="Toggle menu"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 hover:text-gray-200"
+                className="h-6 w-6 hover:text-[#FDFD96]"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -133,27 +127,55 @@ function Navbar() {
 
         {/* Mobile Menu */}
         {isMobileMenuOpen && (
-          <div className="md:hidden bg-gray-800 text-white">
+          <div className="md:hidden bg-[#006D5B] text-center text-yellow-400">
             <ul className="px-4 py-6 space-y-4">
               <li>
-                <a className="block hover:text-gray-400" href="#">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "block text-black text-lg bg-blue-400"
+                      : "block text-yellow-400 text-lg "
+                  }
+                >
                   Home
-                </a>
+                </NavLink>
               </li>
               <li>
-                <a className="block hover:text-gray-400" href="#">
-                  Category
-                </a>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "block text-black text-lg bg-blue-400"
+                      : "block text-yellow-400 text-lg "
+                  }
+                >
+                  About
+                </NavLink>
               </li>
               <li>
-                <a className="block hover:text-gray-400" href="#">
-                  Collections
-                </a>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "block text-black text-lg bg-blue-400"
+                      : "block text-yellow-400 text-lg "
+                  }
+                >
+                  Contact
+                </NavLink>
               </li>
               <li>
-                <a className="block hover:text-gray-400" href="#">
-                  Contact Us
-                </a>
+                <NavLink
+                  to="/my-profile"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "block text-black text-lg bg-blue-400"
+                      : "block text-yellow-400 text-lg "
+                  }
+                >
+                  Profile
+                </NavLink>
               </li>
             </ul>
           </div>
